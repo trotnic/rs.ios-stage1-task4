4bb19e989a3ad6a545285eb5fd96f0f61883833a
Vladislav Volchik <volf10011@gmail.com>
2020-04-12 18:17:03 +0300

    feat: TelephoneFinder done

--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -3,6 +3,68 @@
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
     // good luck
-    return @[@"0"];
+    //
+    
+    if([number intValue] < 0) return nil;
+    
+    NSMutableString *mCopy = [[NSMutableString alloc] initWithString:number];
+    NSMutableArray *result = [NSMutableArray new];
+    
+    for(NSUInteger i = 0; i < [number length]; i++) {
+        NSRange position = NSMakeRange(i, 1);
+        switch ([[number substringWithRange:position] intValue]) {
+            case 0:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"8"]];
+                break;
+            case 1:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"4"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"2"]];
+                break;
+            case 2:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"1"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"5"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"3"]];
+                break;
+            case 3:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"2"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"6"]];
+                break;
+            case 4:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"1"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"5"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"7"]];
+                break;
+            case 5:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"2"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"4"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"6"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"8"]];
+                break;
+            case 6:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"3"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"5"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"9"]];
+                break;
+            case 7:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"4"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"8"]];
+                break;
+            case 8:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"5"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"7"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"9"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"0"]];
+                break;
+            case 9:
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"6"]];
+                [result addObject:[mCopy stringByReplacingCharactersInRange:position withString:@"8"]];
+                break;
+            
+            default:
+                break;
+        }
+    }
+    
+    return [NSArray arrayWithArray:result];
 }
 @end
