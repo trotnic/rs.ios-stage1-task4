0382c6547f75676e0d5d03a248d53be2066fc318
Vladislav Volchik <volf10011@gmail.com>
2020-04-12 18:16:40 +0300

    feat: SquareDecomposer done

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -3,6 +3,27 @@
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
     // good luck
-    return @[@0];
+    // i appreciate this :)
+    
+    if([number intValue] < 5) return nil;
+    NSMutableArray *result = [SquareDecomposer decomposerWithCount:[number intValue] andRemainder:pow([number intValue], 2)];
+    [result removeLastObject];
+    return result;
+}
+
++ (NSMutableArray *)decomposerWithCount:(NSInteger)n andRemainder:(NSInteger)r {
+    if(r == 0)
+        return [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithLong:n], nil];
+    
+    for(NSUInteger i = n - 1; i > 0; i--)
+        if((r - pow(i,2)) >= 0) {
+            NSMutableArray *result = [[NSMutableArray alloc] initWithArray:[self decomposerWithCount:i andRemainder:(r - pow(i,2))]];
+            if([result count] != 0) {
+                [result addObject:[NSNumber numberWithLong:n]];
+                return result;
+            }
+        }
+    
+    return [[NSMutableArray alloc] initWithArray:@[]];
 }
 @end
